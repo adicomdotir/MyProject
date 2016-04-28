27dfb26d94a231844ce10fdc153561df7f1c799b
Adicom <adicom.ir@gmail.com>
2016-04-28 21:51:43 +0430

    Game commit

--- Ad.java
+++ Ad.java
@@ -1,19 +1,32 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Ad {
 	public static void main(String[] args) {
-		System.out.println("Hello Ubuntu!" 
-			+ new Person("Lili") 
-			+ new Person("Ali")
-		);
-	}
-}
-
-class Person {
-	String name;
-	Person(String str) {
-		name = str;
-	}
-	@Override
-	public String toString() {
-		return "Name:" + name;
+		/*
+		ArrayList<String> obj = new ArrayList<String>();
+		obj.add("David");
+		obj.add("Cris");
+		obj.add(0, "Leo");
+		obj.remove(1);
+		obj.remove("Leo");
+		
+		System.out.println("Advanced For Loop"); 		
+		for(String x:obj)
+			System.out.println(x);
+		
+		System.out.println("Iterator is best");
+		Iterator iter = obj.iterator();
+		while (iter.hasNext()) {
+			System.out.println(iter.next());
+		}
+		*/
+		int c=0;
+		for(int i=0; i<1000000; i++) {
+			if(i%1987==0) {
+				c++;
+			}
+		}
+		System.out.println(c);
 	}
 }
