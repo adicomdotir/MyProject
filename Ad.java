c18fa401976b2aa20264659a5b998d17c08e2c1d
Adicom <adicom.ir@gmail.com>
2016-05-27 18:01:22 +0430

    final commit

---
+++ Ad.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Ad {
+	public static void main(String[] args) {
+		int c=0;
+		for(int i=0; i<1000000; i++) {
+			if(i%1987==0) {
+				c++;
+			}
+		}
+		System.out.println(c);
+	}
+}
