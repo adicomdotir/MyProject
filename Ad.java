e2bbd214940d2012634ea9c976a0720d5f16c879
Adicom <adicom.ir@gmail.com>
2016-06-19 11:46:39 +0430

    add after fetch for test

--- Ad.java
+++ Ad.java
@@ -3,6 +3,7 @@ import java.util.Iterator;
 // test
 public class Ad {
 	public static void main(String[] args) {
+		System.out.println("Hello World");
 		int c=0;
 		for(int i=0; i<1000000; i++) {
 			if(i%1366==0) {
