213cdb075418b02596fe157c336176413c635b82
K112 <k112.ard@chmail.ir>
2016-06-30 00:38:03 +0430

    Update Ad.java

--- Ad.java
+++ Ad.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.Iterator;
 // test
+// this section add by k112
+// this is fun
 public class Ad {
 	public static void main(String[] args) {
 		int c=0;
