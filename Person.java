a10bb0972088a888ef9ed28a2c8d0e74bc4d7fb7
Adicom <adicom.ir@gmail.com>
2017-03-23 18:30:15 +0430

    Modified index.html

--- Person.java
+++ Person.java
@@ -1,8 +1,13 @@
+import java.util.Vector;
+
 public class Person implements Worker {
 
   private String name;
 
   private Integer age;
+
+    public Vector  myTest;
+    public Vector  myTest;
 
   public setName(String name) {
   }
