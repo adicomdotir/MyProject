c94b9f7a0be960f267e12786d9e6c8ac6bb8600f
Adicom <adicom.ir@gmail.com>
2017-03-26 21:24:41 +0430

    add point and rectangle

---
+++ Point.java
@@ -0,0 +1,7 @@
+import java.util.Vector;
+
+public class Point {
+
+    public Vector  myRectangle;
+  
+}
---
+++ Rectangle.java
@@ -0,0 +1,8 @@
+import java.util.Vector;
+
+public class Rectangle {
+
+    public Vector  myPoint;
+    public Vector  myPoint;
+
+}
