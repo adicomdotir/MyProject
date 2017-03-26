67c93433f37b7d4a4836be1fae4e3c1d23ff68ef
Adicom <adicom.ir@gmail.com>
2017-03-26 11:05:06 +0430

    Add Company.java files

--- Person.java
+++ Person.java
@@ -1,64 +1,7 @@
+import java.util.Vector;
 
-
-/**
- * Class Person
- */
 public class Person {
 
-  //
-  // Fields
-  //
-
-  private String name;
-  
-  //
-  // Constructors
-  //
-  public Person () { };
-  
-  //
-  // Methods
-  //
-
-
-  //
-  // Accessor methods
-  //
-
-  /**
-   * Set the value of name
-   * @param newVar the new value of name
-   */
-  private void setName (String newVar) {
-    name = newVar;
-  }
-
-  /**
-   * Get the value of name
-   * @return the value of name
-   */
-  private String getName () {
-    return name;
-  }
-
-  //
-  // Other methods
-  //
-
-  /**
-   * @param        name
-   */
-  public void setName(String name)
-  {
-  }
-
-
-  /**
-   * @return       String
-   */
-  public String getName()
-  {
-  }
-
+    public Vector  myCompany;
 
 }
