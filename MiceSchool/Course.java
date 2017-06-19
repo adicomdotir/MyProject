512638590cdab5aec82123cd0625940b73087a44
Adicom <adicom.ir@gmail.com>
2017-06-19 21:44:06 +0430

    Add MicSchool project

---
+++ MiceSchool/Course.java
@@ -0,0 +1,35 @@
+
+
+public class Course {
+	private Student[] students = new Student[10];
+	private String courseName;
+	private int count = 0;
+
+	public boolean register(Student s) {
+		if(count >= 10)
+			return false;
+		else {
+			students[count] = s;
+		}
+		return true;
+	}
+
+	public int getNumOfStudents() {
+		return count;
+	}
+
+	public Student[] getStudents() {
+		return students;
+	}
+
+	public String getName() {
+		return courseName;
+	}
+
+	public void setName(String name) {
+		this.corseName = name;
+	}
+
+	
+}
+
