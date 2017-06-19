edb118f8545b2e323b2956cdd9b749d8a1026b4b
Adicom <adicom.ir@gmail.com>
2017-06-20 00:42:39 +0430

    Delete design pattern

--- BuilderDesignPattern/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-	public static void main(String[] args) {
-		Director d = new Director();
-		ConcreteBuilder1 cb1 = new ConcreteBuilder1();
-		d.construct(cb1);
-		System.out.println(cb1.p);
-	}
-}
