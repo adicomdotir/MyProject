d2651635eda042453aa636b4099bb612a829d6e8
Adicom <adicom.ir@gmail.com>
2017-08-01 00:10:43 +0430

    Add other cpp

--- demo.js
+++ demo.js
@@ -1,9 +1,33 @@
 function myMethod(index) {
     document.writeln("HelloWorld! " + index);
     $("div").click(function () {
-        
+
     });
     $(document).ready(function() {
-        
+
     });
+}
+var test = function() {
+    return
+}
+
+var myVar = "global"; // Declare a global variable
+function checkscope( ) {
+    var myVar = "local";  // Declare a local variable
+    document.write(myVar);
+}
+
+var person = new Object();
+person.name = "Ali";
+person.age = 34;
+
+var person = {
+    name : "Yashar",
+    age = 30
+};
+
+var person = {}; // LIKE = var person = new Object();
+
+class MyClass {
+    
 }
--- hello.cpp
+++ hello.cpp
@@ -3,5 +3,15 @@
 int main(void) 
 {
     std::cout << "Hello! This is a C++ program.\n";
+    for(int i=0; i<10; i++) {
+    	std::cout << i << "\n";
+    }
+    MyClass mClass = new MyClass();
     return 0;
+}
+
+class MyClass {
+	MyClass() {
+		std::cout << "Constructor";
+	}
 }
