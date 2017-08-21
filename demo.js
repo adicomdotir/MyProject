e189e4c9f04be5a62f53d6e8c78b65255c4147a2
Adicom <adicom.ir@gmail.com>
2017-08-21 18:29:40 +0430

    Add javacup-company-session-7

--- demo.js
+++ demo.js
@@ -1,33 +1,4 @@
-function myMethod(index) {
-    document.writeln("HelloWorld! " + index);
-    $("div").click(function () {
-
-    });
-    $(document).ready(function() {
-
-    });
-}
-var test = function() {
-    return
-}
-
-var myVar = "global"; // Declare a global variable
-function checkscope( ) {
-    var myVar = "local";  // Declare a local variable
-    document.write(myVar);
-}
-
-var person = new Object();
-person.name = "Ali";
-person.age = 34;
-
-var person = {
-    name : "Yashar",
-    age = 30
-};
-
-var person = {}; // LIKE = var person = new Object();
-
-class MyClass {
-    
+// میتوان یک عبارت تابعی را به یک شی نسبت داد
+var sum = function(a, b) {
+    return a + b;
 }
