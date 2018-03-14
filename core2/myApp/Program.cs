20e2f9312f915ad5cdfc16a0a754af33db61c477
Adicom.ir <adicom.ir@gmail.com>
2018-03-14 23:15:07 +0330

    Bash programming

--- core2/myApp/Program.cs
+++ core2/myApp/Program.cs
@@ -7,11 +7,22 @@ namespace myApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!" + sum(5, 9));
-
+			var p = new Person() {
+				name = "Ali",
+				age = 15
+			};
+			Console.WriteLine(p.name + " : " + p.age);
         }
 
         static int sum(int x, int y) {
             return x * y;
         }
+    }
+    
+    class Person {
+    	public string name { set; get; }
+    	public int age { set; get; }
+    	Public Person() {
+    	}
     }
 }
