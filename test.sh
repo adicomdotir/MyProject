1e18a474980f05c47e54038512c6b42cdaf65315
Adicom.ir <adicom.ir@gmail.com>
2018-03-15 00:04:33 +0330

    Fix convention

--- test.sh
+++ test.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
 read -p "What is your name? " name
 echo "Hello $name"
-if [ "$name" == "adicom" ]
-	then
-		echo "You are admin!"
-	else
-		echo "You are ghost"
-		exit
+if [ "$name" == "adicom" ];then
+	echo "You are admin!"
+else
+	echo "You are ghost"
+	exit
 fi
 ls
 read -p "Please enter a directory name : " dir
-if [ -d "$HOME/$dir" ]
-	then
-		echo "Exist!"
+if [ -d "$HOME/$dir" ];then
+	echo "Exist!"
+else
+	echo -e "Directory not existed\nDo you want create $dir (y/n)?"
+	read answer
+	if [ "$answer" == "y" ];then
+		mkdir "$directory"
 	else
-		echo -e "Directory not existed\nDo you want create $dir (y/n)?"
-		read answer
-		if [ "$answer" == "y" ]
-			then
-				mkdir "$directory"
-			else
-				echo "Bye!"
-		fi
+		echo "Bye!"
+	fi
 fi
 
