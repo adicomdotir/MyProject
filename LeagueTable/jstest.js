3ea7b2661b20aba0eff56aff2c95cbf7909fe430
Adicom.ir <adicom.ir@gmail.com>
2018-04-05 23:32:22 +0430

    Add js google convention

---
+++ LeagueTable/jstest.js
@@ -0,0 +1,52 @@
+// bad
+[1, 2, 3].map(function (x) {
+  const y = x + 1;
+  return x * y;
+});
+
+// good
+[1, 2, 3].map((x) => {
+  const y = x + 1;
+  return x * y;
+});
+
+// bad
+function sayHi(name) {
+  return 'How are you, ' + name + '?';
+}
+
+// bad
+function sayHi(name) {
+  return ['How are you, ', name, '?'].join();
+}
+
+// bad
+function sayHi(name) {
+  return `How are you, ${ name }?`;
+}
+
+// good
+function sayHi(name) {
+  return `How are you, ${name}?`;
+}
+
+// bad
+const number = 5;
+// good
+const NUMBER = 5;
+
+// bad
+let a = 1, b = 2, c = 3;
+// good
+let a = 1;
+let b = 2;
+let c = 3;
+
+// bad
+let directive = "No identification of self or mission."
+// bad
+let saying = 'Say it ain\u0027t so.';
+// good
+let directive = 'No identification of self or mission.';
+// good
+let saying = `Say it ain't so`;
