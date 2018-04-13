e0563e85456bbbde309058612a1900f13a581fde
Adicom.ir <adicom.ir@gmail.com>
2018-04-13 17:34:04 +0430

    Clean code for league table

--- LeagueTable/custom.js
+++ LeagueTable/custom.js
@@ -1,20 +1,14 @@
-var teamsInfo = [];
-var size = teamsInfo.length;
-var teams = [];
+let teamsInfo = [];
+let size = 0;
+let teams = [];
 
 $(document).ready(function() {
+	init();
+	gameCycle();
+});
 
-	// var barca = new Team('Barcelona', 85);
-	// var rm = new Team('RealMadrid', 86);
-	// var psg = new Team('PSG', 83);
-	// var chelsea = new Team('Chelsea', 83);
-	// var inter = new Team('Inter', 81);
-	// var milan = new Team('Milan', 79);
-	// var t1 = new Team('SL Benfica', 79);
-	// var t2 = new Team('FC Porto', 79);
-	// var t3 = new Team('SC Braga', 75);
-	// var t4 = new Team('Sporting CP', 78);
-	// teamsInfo.push(barca, rm, psg, chelsea, inter, milan);
+function init() {
+	// Generate teams
 	for (let index = 0; index < 6; index++) {
 		const teamName = `Team${index + 1}`;
 		const overall = Math.round(Math.random() * 20 + 60);
@@ -23,118 +17,12 @@ $(document).ready(function() {
 	}
 
 	size = teamsInfo.length;
-	for (var i = 0; i < size; i++) {
+
+	// for roundrobin algorithm
+	for (let i = 0; i < size; i++) {
 		teams[i] = i;
 	}
-	var col = 0;
-	for (var i = 1; i < size * 2 - 1; i++) {
-		var str = '';
-		var header = document.createElement("div");
-		// var header = $("div");
-		$(header).append('<b>' + "Week " + numberToText(i) + '</b>');
-		if (col === 0) {
-			$('#figure3').append(header);
-		} else if (col === 1) {
-			$('#figure2').append(header);
-		} else if (col === 2) {
-			$('#figure').append(header);
-		}
-		for (var j = 0; j < size / 2; j++) {
-			var diff = teamsInfo[teams[j]].overall - teamsInfo[teams[size - j - 1]].overall;
-			var mulA = 0,
-				mulB = 0;
-			if (diff > 0) {
-				mulA = diff / 2;
-				mulB = diff / 4;
-			} else if (diff < 0) {
-				mulA = diff / 4;
-				mulB = diff / 2;
-			}
-			mulA = Math.floor(Math.abs(mulA));
-			mulB = Math.floor(Math.abs(mulB));
-			var gA = Math.floor(Math.random() * (mulA + 1));
-			var gB = Math.floor(Math.random() * (mulB + 1));
-			// console.log(mulA + '[' + gA + ':' + gB + ']' + mulB);
-			// console.log(gA);
-			if (gA === gB) {
-				teamsInfo[teams[j]].game++;
-				teamsInfo[teams[j]].draw++;
-				teamsInfo[teams[j]].points++;
-				teamsInfo[teams[j]].gf += gA;
-				teamsInfo[teams[j]].ga += gB;
-				teamsInfo[teams[size - j - 1]].game++;
-				teamsInfo[teams[size - j - 1]].draw++;
-				teamsInfo[teams[size - j - 1]].points++;
-				teamsInfo[teams[size - j - 1]].gf += gB;
-				teamsInfo[teams[size - j - 1]].ga += gA;
-			} else if (gA > gB) {
-				teamsInfo[teams[j]].game++;
-				teamsInfo[teams[j]].win++;
-				teamsInfo[teams[j]].points += 3;
-				teamsInfo[teams[j]].gf += gA;
-				teamsInfo[teams[j]].ga += gB;
-				teamsInfo[teams[size - j - 1]].game++;
-				teamsInfo[teams[size - j - 1]].lose++;
-				teamsInfo[teams[size - j - 1]].gf += gB;
-				teamsInfo[teams[size - j - 1]].ga += gA;
-			} else {
-				teamsInfo[teams[j]].game++;
-				teamsInfo[teams[j]].lose++;
-				teamsInfo[teams[j]].gf += gA;
-				teamsInfo[teams[j]].ga += gB;
-				teamsInfo[teams[size - j - 1]].game++;
-				teamsInfo[teams[size - j - 1]].win++;
-				teamsInfo[teams[size - j - 1]].points += 3;
-				teamsInfo[teams[size - j - 1]].gf += gB;
-				teamsInfo[teams[size - j - 1]].ga += gA;
-			}
-			var div1 = document.createElement("div");
-			addAttributeColor(gA, gB, div1);
-			$(div1).append(teamsInfo[teams[j]].name).addClass('col-md-4');
-			var div2 = document.createElement("div");
-			$(div2).append(gA + '-' + gB).addClass('col-md-4');
-			var div3 = document.createElement("div");
-			addAttributeColor(gB, gA, div3);
-			$(div3).append(teamsInfo[teams[size - j - 1]].name).addClass('col-md-4');
-			if (col === 0) {
-				$('#figure3').append(div1);
-				$('#figure3').append(div2);
-				$('#figure3').append(div3);
-			} else if (col === 1) {
-				$('#figure2').append(div1);
-				$('#figure2').append(div2);
-				$('#figure2').append(div3);
-			} else if (col === 2) {
-				$('#figure').append(div1);
-				$('#figure').append(div2);
-				$('#figure').append(div3);
-			}
-		}
-		// console.log(col);
-		col++;
-		if (col === 3) {
-			col = 0;
-		}
-		swap();
-	}
-	sort();
-
-	// console.log(teamsInfo);
-	for (var i = 0; i < size; i++) {
-		var row = document.createElement("tr");
-		$(row).append('<td>' + (i + 1) + '</td>');
-		for (var key in teamsInfo[i]) {
-			if (key === 'gd') {
-				$(row).append('<td>' + (teamsInfo[i]['gf'] - teamsInfo[i]['ga']) + '</td>');
-			} else if (key === 'name') {
-				$(row).append('<td>' + (teamsInfo[i][key]) + '[' + teamsInfo[i]['overall'] + ']' + '</td>');
-			} else if (key !== 'overall') {
-				$(row).append('<td>' + (teamsInfo[i][key]) + '</td>');
-			}
-		}
-		$('#tbody').append(row);
-	}
-});
+}
 
 function Team(name, overall) {
 	this.name = name;
@@ -150,8 +38,8 @@ function Team(name, overall) {
 }
 
 function swap() {
-	var temp = teams[size - 1];
-	for (var i = size - 1; i > 0; i--) {
+	let temp = teams[size - 1];
+	for (let i = size - 1; i > 0; i--) {
 		teams[i] = teams[i - 1];
 	}
 	teams[1] = temp;
@@ -159,27 +47,27 @@ function swap() {
 
 function sort() {
 	// For calculate Goal Different
-	for (var i = 0; i < size; i++) {
+	for (let i = 0; i < size; i++) {
 		teamsInfo[i].gd = teamsInfo[i].gf - teamsInfo[i].ga;
 	}
 	// This loop for sort points
-	for (var i = 0; i < size; i++) {
-		for (var j = i + 1; j < size; j++) {
+	for (let i = 0; i < size; i++) {
+		for (let j = i + 1; j < size; j++) {
 			if (teamsInfo[j].points > teamsInfo[i].points) {
-				var temp = teamsInfo[j];
+				let temp = teamsInfo[j];
 				teamsInfo[j] = teamsInfo[i];
 				teamsInfo[i] = temp;
 			}
 		}
 	}
 	// This loop for sort GD
-	for (var i = 0; i < size; i++) {
-		for (var j = i + 1; j < size; j++) {
+	for (let i = 0; i < size; i++) {
+		for (let j = i + 1; j < size; j++) {
 			if (teamsInfo[j].points === teamsInfo[i].points) {
 				// console.log(teamsInfo[j].points + '===' + teamsInfo[i].points);
 				if (teamsInfo[j].gd > teamsInfo[i].gd) {
 					// console.log(teamsInfo[j].gd + '>' + teamsInfo[i].gd);
-					var temp = teamsInfo[j];
+					let temp = teamsInfo[j];
 					teamsInfo[j] = teamsInfo[i];
 					teamsInfo[i] = temp;
 				}
@@ -187,15 +75,15 @@ function sort() {
 		}
 	}
 	// This loop for sort GF
-	for (var i = 0; i < size; i++) {
-		for (var j = i + 1; j < size; j++) {
+	for (let i = 0; i < size; i++) {
+		for (let j = i + 1; j < size; j++) {
 			if (teamsInfo[j].points === teamsInfo[i].points) {
 				// console.log(teamsInfo[j].points + '===' + teamsInfo[i].points);
 				if (teamsInfo[j].gd === teamsInfo[i].gd) {
 					// console.log(teamsInfo[j].gd + '===' + teamsInfo[i].gd);
 					if (teamsInfo[j].gf > teamsInfo[i].gf) {
 						// console.log(teamsInfo[j].gf + '>' + teamsInfo[i].gf);
-						var temp = teamsInfo[j];
+						let temp = teamsInfo[j];
 						teamsInfo[j] = teamsInfo[i];
 						teamsInfo[i] = temp;
 					}
@@ -210,8 +98,8 @@ function numberToText(num) {
 		return 'Error';
 	}
 	if (num > 20 && num % 10 !== 0) {
-		var n1 = num % 10;
-		var n2 = Math.floor(num / 10) * 10;
+		let n1 = num % 10;
+		let n2 = Math.floor(num / 10) * 10;
 		return numberToText(n2) + numberToText(n1);
 	}
 	switch (num) {
@@ -279,5 +167,123 @@ function addAttributeColor(gObj, gOther, obj) {
 		$(obj).css("color", "#E3000E");
 	} else {
 		$(obj).css("color", "#FEC606");
+	}
+}
+
+function createTeble() {
+	for (let i = 0; i < size; i++) {
+		let row = document.createElement("tr");
+		$(row).append('<td>' + (i + 1) + '</td>');
+		for (let key in teamsInfo[i]) {
+			if (key === 'gd') {
+				$(row).append('<td>' + (teamsInfo[i]['gf'] - teamsInfo[i]['ga']) + '</td>');
+			} else if (key === 'name') {
+				$(row).append('<td>' + (teamsInfo[i][key]) + '[' + teamsInfo[i]['overall'] + ']' + '</td>');
+			} else if (key !== 'overall') {
+				$(row).append('<td>' + (teamsInfo[i][key]) + '</td>');
+			}
+		}
+		$('#tbody').append(row);
+	}
+}
+
+function gameCycle() {
+	let col = 0;
+	for (let i = 1; i < size * 2 - 1; i++) {
+		let str = '';
+		let header = document.createElement("div");
+		// let header = $("div");
+		$(header).append('<b>' + "Week " + numberToText(i) + '</b>');
+		if (col === 0) {
+			$('#figure3').append(header);
+		} else if (col === 1) {
+			$('#figure2').append(header);
+		} else if (col === 2) {
+			$('#figure').append(header);
+		}
+		for (let j = 0; j < size / 2; j++) {
+			let diff = teamsInfo[teams[j]].overall - teamsInfo[teams[size - j - 1]].overall;
+			let mulA = 0,
+				mulB = 0;
+			if (diff > 0) {
+				mulA = diff / 2;
+				mulB = diff / 4;
+			} else if (diff < 0) {
+				mulA = diff / 4;
+				mulB = diff / 2;
+			}
+			mulA = Math.floor(Math.abs(mulA));
+			mulB = Math.floor(Math.abs(mulB));
+			let gA = Math.floor(Math.random() * (mulA + 1));
+			let gB = Math.floor(Math.random() * (mulB + 1));
+
+			updateTeamInfo(j, gA, gB);
+			
+			let div1 = document.createElement("div");
+			addAttributeColor(gA, gB, div1);
+			$(div1).append(teamsInfo[teams[j]].name).addClass('col-md-4');
+			let div2 = document.createElement("div");
+			$(div2).append(gA + '-' + gB).addClass('col-md-4');
+			let div3 = document.createElement("div");
+			addAttributeColor(gB, gA, div3);
+			$(div3).append(teamsInfo[teams[size - j - 1]].name).addClass('col-md-4');
+			if (col === 0) {
+				$('#figure3').append(div1);
+				$('#figure3').append(div2);
+				$('#figure3').append(div3);
+			} else if (col === 1) {
+				$('#figure2').append(div1);
+				$('#figure2').append(div2);
+				$('#figure2').append(div3);
+			} else if (col === 2) {
+				$('#figure').append(div1);
+				$('#figure').append(div2);
+				$('#figure').append(div3);
+			}
+		}
+		col++;
+		if (col === 3) {
+			col = 0;
+		}
+		swap();
+	}
+
+	sort();
+
+	createTeble();
+}
+
+function updateTeamInfo(index, goalA, goalB) {
+	if (goalA === goalB) {
+		teamsInfo[teams[index]].game++;
+		teamsInfo[teams[index]].draw++;
+		teamsInfo[teams[index]].points++;
+		teamsInfo[teams[index]].gf += goalA;
+		teamsInfo[teams[index]].ga += goalB;
+		teamsInfo[teams[size - index - 1]].game++;
+		teamsInfo[teams[size - index - 1]].draw++;
+		teamsInfo[teams[size - index - 1]].points++;
+		teamsInfo[teams[size - index - 1]].gf += goalB;
+		teamsInfo[teams[size - index - 1]].ga += goalA;
+	} else if (goalA > goalB) {
+		teamsInfo[teams[index]].game++;
+		teamsInfo[teams[index]].win++;
+		teamsInfo[teams[index]].points += 3;
+		teamsInfo[teams[index]].gf += goalA;
+		teamsInfo[teams[index]].ga += goalB;
+		teamsInfo[teams[size - index - 1]].game++;
+		teamsInfo[teams[size - index - 1]].lose++;
+		teamsInfo[teams[size - index - 1]].gf += goalB;
+		teamsInfo[teams[size - index - 1]].ga += goalA;
+	} else {
+		teamsInfo[teams[index]].game++;
+		teamsInfo[teams[index]].lose++;
+		teamsInfo[teams[index]].gf += goalA;
+		teamsInfo[teams[index]].ga += goalB;
+		teamsInfo[teams[size - index - 1]].game++;
+		teamsInfo[teams[size - index - 1]].win++;
+		teamsInfo[teams[size - index - 1]].points += 3;
+		teamsInfo[teams[size - index - 1]].gf += goalB;
+		teamsInfo[teams[size - index - 1]].ga += goalA;
 	}
 }
