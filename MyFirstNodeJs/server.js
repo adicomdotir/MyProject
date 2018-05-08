090f207787029662f9a6073da13c8426f8a2bf61
Adicom.ir <adicom.ir@gmail.com>
2018-05-08 23:12:56 +0430

    Add new nodejs feature

--- MyFirstNodeJs/server.js
+++ MyFirstNodeJs/server.js
@@ -1,18 +1,17 @@
 var http = require('http');
 var url = require('url');
 
-function start(route) {
+function start(route, handle) {
     function onRequest(request, response) {
         var pathname = url.parse(request.url).pathname;
-        console.log('Request for' + pathname + ' received.');
-
-        route(pathname);
+        console.log('Request for ' + pathname + ' received.');
 
         response.writeHead(200, {'Content-Type': 'text/plain'});
-        response.write('Hello World');
+        var content = route(handle, pathname);
+        response.write('content');
         response.end();
     }
-    http.createServer(onRequest).listen(4567);
+    http.createServer(onRequest).listen(9009);
     console.log('Server has started.');
 }
 
