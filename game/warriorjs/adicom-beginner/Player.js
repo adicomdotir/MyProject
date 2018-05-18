0c34cb881cffece3ff824237bb77b9622dcd9ed7
Adicom.ir <adicom.ir@gmail.com>
2018-05-18 11:17:07 +0430

    Add level four

--- game/warriorjs/adicom-beginner/Player.js
+++ game/warriorjs/adicom-beginner/Player.js
@@ -1,13 +1,21 @@
 class Player {
+  
+  constructor() {
+    this.health = 0;
+  }
+
   playTurn(warrior) {
     if (warrior.feel().isUnit()) {
         warrior.attack();
     } else {
-      if (warrior.health() < 20) {
+      if (warrior.health() < this.health) {
+        warrior.walk();
+      } else if (warrior.health() < 20) {
         warrior.rest();
       } else {
         warrior.walk();
       }
+      this.health = warrior.health();
     }
   }
 }
