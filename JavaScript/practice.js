13ec6e999b91dfc04effd8fedac2ee201acf34ba
Adicom.ir <adicom.ir@gmail.com>
2018-05-23 18:53:13 +0430

    Add new test for js

--- JavaScript/practice.js
+++ JavaScript/practice.js
@@ -1,63 +1,85 @@
-function User(){
-	var username, password;
-	function doLogin(user,pw) {
-		username = user;
-		password = pw;
+const users = [
+	{
+		name: 'Ali',
+		age: 25
+	},
+	{
+		name: 'Hassan',
+		age: 29
+	},
+	{
+		name: 'Yaser',
+		age: 20
+	},
+];
+users.forEach((user) => {
+	console.log(user);
+});
 
-		// do the rest of the login work
-	}
-	var publicAPI = {
-		login: doLogin
-	};
-	return publicAPI;
-}
+const user2 = users.filter((user) => {
+	return user.age <= 25
+});
+console.log(user2);
+// function User(){
+// 	var username, password;
+// 	function doLogin(user,pw) {
+// 		username = user;
+// 		password = pw;
 
-// create a `User` module instance
-var fred = User();
-fred.login( "adicom", "12Battery34!" );
+// 		// do the rest of the login work
+// 	}
+// 	var publicAPI = {
+// 		login: doLogin
+// 	};
+// 	return publicAPI;
+// }
 
-//--------------------//
-(function IIFE(){
-	console.log( "Hello!" );
-})();
-// "Hello!"
+// // create a `User` module instance
+// var fred = User();
+// fred.login( "adicom", "12Battery34!" );
 
-function makeNum(x) {
-	function add(y) {
-		return y + x;
-	};
-	return add;
-}
+// //--------------------//
+// (function IIFE(){
+// 	console.log( "Hello!" );
+// })();
+// // "Hello!"
 
-var addOne = makeNum(1);
-var addTen = makeNum(10);
+// function makeNum(x) {
+// 	function add(y) {
+// 		return y + x;
+// 	};
+// 	return add;
+// }
 
-addOne(3);		// 4  <-- 1 + 3
-addOne(41);		// 42 <-- 1 + 41
-addTen(13);		// 23 <-- 10 + 13
+// var addOne = makeNum(1);
+// var addTen = makeNum(10);
 
-// Closure Example
-function sayHello(name) {
-	var text = 'Hello ' + name;
-	var say = function() { console.log(text); }
-	say();
-}
-sayHello('Adicom');
+// addOne(3);		// 4  <-- 1 + 3
+// addOne(41);		// 42 <-- 1 + 41
+// addTen(13);		// 23 <-- 10 + 13
 
-function sayHello2(name) {
-	var text = 'Hello ' + name; // Local variable
-	var say = function() { console.log(text); }
-	return say;
-}
-var say2 = sayHello2('Adicom');
-say2(); // logs "Hello Adicom"
+// // Closure Example
+// function sayHello(name) {
+// 	var text = 'Hello ' + name;
+// 	var say = function() { console.log(text); }
+// 	say();
+// }
+// sayHello('Adicom');
 
-function say667() {
-	// Local variable that ends up within closure
-	var num = 42;
-	var say = function() { console.log(num); }
-	num++;
-	return say;
-}
-var sayNumber = say667();
-sayNumber(); // logs 43
+// function sayHello2(name) {
+// 	var text = 'Hello ' + name; // Local variable
+// 	var say = function() { console.log(text); }
+// 	return say;
+// }
+// var say2 = sayHello2('Adicom');
+// say2(); // logs "Hello Adicom"
+
+// function say667() {
+// 	// Local variable that ends up within closure
+// 	var num = 42;
+// 	var say = function() { console.log(num); }
+// 	num++;
+// 	return say;
+// }
+// var sayNumber = say667();
+// sayNumber(); // logs 43
--- java/Main02.java
+++ java/Main02.java
@@ -3,8 +3,10 @@ import java.util.Scanner;
 public class Main02 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int p = sc.nextInt();
-		int d = sc.nextInt();
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		int x1 = sc.nextInt();
+		int y2 = sc.nextInt();
 		if (x < x1) System.out.println("Right");
 		if (x > x1) System.out.println("Left");
 	}
