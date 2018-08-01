eb8269722ce480e8c4ef2df604fd1df515180ba7
Adicom.ir <adicom.ir@gmail.com>
2018-08-01 21:23:28 +0430

    addition for webpack

--- WebpackTraining/webpack.config.js
+++ WebpackTraining/webpack.config.js
@@ -5,5 +5,13 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  // Add the JSHint loader
+  module: {
+    rules: [{
+      test: /.js$/, // Run the loader on all .js files
+      exclude: /node_modules/, // ignore all files in the node_modules folder
+      use: 'jshint-loader'
+    }]
   }
 };
