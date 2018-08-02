c5b667532a3937e30cd79392c834b34818755cb3
Adicom.ir <adicom.ir@gmail.com>
2018-08-02 11:06:16 +0430

    final training

--- WebpackTraining/webpack.config.js
+++ WebpackTraining/webpack.config.js
@@ -1,17 +1,9 @@
-var path = require('path');
+const path = require('path');
 
 module.exports = {
-  entry: './assets/js/index.js',
+  entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'index.bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  // Add the JSHint loader
-  module: {
-    rules: [{
-      test: /.js$/, // Run the loader on all .js files
-      exclude: /node_modules/, // ignore all files in the node_modules folder
-      use: 'jshint-loader'
-    }]
-  }
 };
