9571bf2457c1cde9de6f1a7241e5342ca6a9a415
Adicom.ir <adicom.ir@gmail.com>
2018-09-02 22:02:26 +0430

    Add csharp example

---
+++ C# Examples/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+			A[] elements = new A[] { new D(), new A(), new C(), new B() };
+			for (int i = 0; i < elements.Length; i++) {
+				Console.WriteLine(elements[i].method1());
+				Console.WriteLine(elements[i].method2());
+				Console.WriteLine();
+			}
+        }
+    }
+	
+	class A
+	{
+		public virtual string method1() 
+		{
+			return "A1";
+		}
+		
+		public virtual string method2() 
+		{
+			return "A2";
+		}
+	}
+	
+	class B : A
+	{	
+		public override string method2() 
+		{
+			return method1() + "B2";
+		}
+	}
+	
+	class C : B
+	{	
+		public override string method1() 
+		{
+			return "C1";
+		}
+	}
+	
+	class D : B
+	{	
+		public override string method1() 
+		{
+			return "D1";
+		}
+	}
+}
